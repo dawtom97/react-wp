68bc05597b8ece15253beff2d6330d4ecf19a860
dawid <dawtom97@gmail.com>
2024-10-30 16:24:29 +0100

    env added

--- src/utils/axiosClient.js
+++ src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost/headless/wp-json/wp/v2/", // Zastąp tym adresem URL swoim API
+  baseURL: import.meta.env.VITE_WP_URL, // Zastąp tym adresem URL swoim API
   timeout: 120000, // opcjonalnie: czas oczekiwania na odpowiedź w milisekundach
   headers: {
     'Content-Type': 'application/json',
